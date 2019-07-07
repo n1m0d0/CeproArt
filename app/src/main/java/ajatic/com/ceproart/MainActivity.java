c447c0cd1c649d04127b49645acb8b4a04589c20
n1m0d0 <vascojohn@gmail.com>
2019-07-07 12:48:33 -0400

    inicio de la aplicacion

---
+++ app/src/main/java/ajatic/com/ceproart/MainActivity.java
@@ -0,0 +1,13 @@
+package ajatic.com.ceproart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
